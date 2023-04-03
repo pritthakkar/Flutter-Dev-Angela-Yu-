6fd75afa80848046324d2b953148f3ae165511a9
Prit Thakkar <84337664+pritthakkar@users.noreply.github.com>
2023-04-03 12:12:15 +0530

    Update question.dart

--- Quizzler/question.dart
+++ Quizzler/question.dart
@@ -1,1 +1,12 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
 
+  Question(String q, bool a) {
+
+    questionText = q;
+    questionAnswer = a;
+  }
+}
+
+Question newQuestion = Question('text', true);
